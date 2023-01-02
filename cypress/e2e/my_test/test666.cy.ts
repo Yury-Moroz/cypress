0267b8817daaecb52ff2fa8b45ed117e0d22d707
yury-moroz <yurymoroz89@gmail.com>
2023-01-02 12:44:00 +0300

    add

--- cypress/e2e/my_test/test666.cy.ts
+++
@@ -1,9 +0,0 @@
-describe("test API Swapi", () => {
-  it("get test", () => {
-    cy.request("get", "https://swapi.dev/api/people/1/").then((response) => {
-      expect(response).to.have.property("status", 200);
-      expect(response.body).to.not.be.null;
-      expect(response.body.films).to.have.length(4);
-    });
-  });
-});
